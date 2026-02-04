74335aeef9d4e4fe44d7d9cd910f9bfcd299c8a8
harmog <harmog@foxmail.com>
2026-02-04 11:28:35 +0800

    fix: replace inline env variables when bun build

--- server.ts
+++ server.ts
@@ -1,11 +1,14 @@
 import { Elysia } from "elysia";
 import { staticPlugin } from "@elysiajs/static";
 import { server } from "./server/index";
-
+import index from "public/index.html";
 new Elysia()
+  .get("/", index)
   .use(
     await staticPlugin({
       prefix: "/",
+      indexHTML: false,
+      alwaysStatic: true,
     }),
   )
   .use(server)
